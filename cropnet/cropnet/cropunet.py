7b35bb63fbc5fd364738b4de86ca6e5021ca4a5b
Matt Phillips <matt.phillips@kitware.com>
2018-11-27 20:24:34 -0500

    Working CropUNet model

--- cropnet/cropnet/cropunet.py
+++ cropnet/cropnet/cropunet.py
@@ -103,10 +103,10 @@ class CropUNet(nn.Module):
         self._calc_layer_shapes()
         
         is_deconv = False # TernausNetV2 default
-        num_filters = 64 #32
+        num_filters = 32
         center_sz = self._out_shapes[-1][0]
         self.center = DecoderBlock(center_sz, num_filters*8, num_filters*8,
-                is_deconv=is_deconv, scale_factor=7)
+                is_deconv=is_deconv, scale_factor=1)
         self.dec5 = DecoderBlock(center_sz + num_filters*8, num_filters*8,
                 num_filters*8, is_deconv=is_deconv)
         self.dec4 = DecoderBlock(center_sz//2 + num_filters*8, num_filters*8,
@@ -115,11 +115,11 @@ class CropUNet(nn.Module):
                 num_filters*2, is_deconv=is_deconv)
         self.dec2 = DecoderBlock(center_sz//8 + num_filters*2, num_filters*2,
                 num_filters, is_deconv=is_deconv)
-        self.dec1 = ConvRelu(center_sz//8 + num_filters, num_filters)
-#        self.dec1 = ConvRelu(center_sz//16 + num_filters, num_filters)
+        self.dec1 = DecoderBlock(center_sz//8 + num_filters, num_filters,
+                num_filters, is_deconv=is_deconv)
+#        self.dec1 = ConvRelu(center_sz//8 + num_filters, num_filters)
         self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
         self._decoder = OrderedDict()
-        self._decoder["center"] = self.center
         self._decoder["dec5"] = self.dec5
         self._decoder["dec4"] = self.dec4
         self._decoder["dec3"] = self.dec3
@@ -138,8 +138,6 @@ class CropUNet(nn.Module):
         layer4 = self._encoder["layer4"](layer3)
         avgpool = self._encoder["avgpool"](layer4)
 
-        print("Avgpool shape: %s" % repr(avgpool.shape))
-#        center = self.center(self.pool(avgpool))
         center = self.center(layer4)
 
         dec5 = self.dec5(torch.cat([center, layer4], 1))
@@ -158,7 +156,7 @@ class CropUNet(nn.Module):
             x = module(x)
             self._out_shapes.append(x.shape[1:])
         
-def _test_show_layers(tnet):
+def _test_show_layers(fp, tnet):
     x = torch.FloatTensor(1,3,224,224)
     convs = []
     for name,m in tnet._encoder.items():
@@ -168,7 +166,17 @@ def _test_show_layers(tnet):
         if name=="relu" or name.startswith("layer"):
             convs.append( x.clone() )
         out_shape = x.shape
-        print("%s, In: %s, Out: %s" % (name, repr(in_shape), repr(out_shape)))
+        s = "%s, In: %s, Out: %s" % (name, repr(in_shape), repr(out_shape))
+        print(s)
+        fp.write(s + "\n")
+
+    in_shape = x.shape
+    x = tnet.center(x)
+    out_shape = x.shape
+    s = "%s, In: %s, Out: %s" % ("center", repr(in_shape), repr(out_shape))
+    print(s)
+    fp.write(s + "\n")
+
     convs = convs[::-1]
     deconv_mods = [tnet.dec5,tnet.dec4,tnet.dec3,tnet.dec2,tnet.dec1,tnet.final]
     for i,m in enumerate(deconv_mods):
@@ -177,11 +185,12 @@ def _test_show_layers(tnet):
             x = m(x)
             name = "final"
         else:
-#            print(x.shape, torch.cat([x, convs[i]], 1).shape)
-#            print(m.in_channels)
             x = m( torch.cat([x, convs[i]], 1) )
             name = "dec%d" % (5-i)
-        print("%s, In: %s, Out: %s" % (name, repr(in_shape), repr(out_shape)))
+        out_shape = x.shape
+        s = "%s, In: %s, Out: %s" % (name, repr(in_shape), repr(out_shape))
+        print(s)
+        fp.write(s + "\n")
 
 
 def _main(args):
@@ -195,39 +204,12 @@ def _main(args):
     with open(pj(output_dir, "crop_unet_model.txt"), "w") as fp:
         fp.write(repr(crop_unet) + "\n")
 
-    _test_show_layers(crop_unet)
-    raise
+    with open( pj(output_dir, "crop_unet_layers.txt"), "w") as fp:
+        _test_show_layers(fp, crop_unet)
 
-    sz = (3, 224, 224)
-    x = torch.FloatTensor(1, *sz)
-    specs = OrderedDict()
-    for m in crop_unet._resnet.named_children():
-        name,layers = m
-        in_shape = x.shape
-        if name == "fc":
-            x = x.view(x.size(0), -1)
-        x = layers(x)
-        out_shape = x.shape
-        print("%s, In: %s, Out: %s" % (name, repr(list(in_shape)),
-            repr(list(out_shape))))
-        specs[name] = { "in_shape" : in_shape, "out_shape" : out_shape }
-        # TODO print out tensor input, output dimensions of each top-level block
-
-    xd = torch.FloatTensor(1, 512+256, 256, 256)
-    for name in ["dec5","dec4","dec3","dec2", "dec1","final"]:
-        m = crop_unet._decoder[name]
-        in_shape = xd.shape
-        xd = m(xd)
-        out_shape = xd.shape
-        print("%s, In: %s, Out: %s" % (name, repr(list(in_shape)),
-            repr(list(out_shape))))
-
-    with open(pj(output_dir, "crop_unet_model.txt"), "w") as fp:
-        json.dump(specs, fp, indent=2)
-
-    x = torch.FloatTensor(1, *sz)
+    x = torch.FloatTensor(1,3,224,224)
     y = crop_unet(x)
-    print(y.shape, type(y))
+    print("Successfully executed model 'forward' method")
     
 
 if __name__ == "__main__":
@@ -238,31 +220,4 @@ if __name__ == "__main__":
             choices=["resnet18", "resnet34", "resnet50"])
     args = parser.parse_args()
     _main(args)
-
-
-#        self.center = DecoderBlock(1024, num_filters * 8, num_filters * 8, is_deconv=is_deconv)
-#        self.dec5 = DecoderBlock(1024 + num_filters * 8, num_filters * 8, num_filters * 8, is_deconv=is_deconv)
-#        self.dec4 = DecoderBlock(512 + num_filters * 8, num_filters * 8, num_filters * 8, is_deconv=is_deconv)
-#        self.dec3 = DecoderBlock(256 + num_filters * 8, num_filters * 2, num_filters * 2, is_deconv=is_deconv)
-#        self.dec2 = DecoderBlock(128 + num_filters * 2, num_filters * 2, num_filters, is_deconv=is_deconv)
-#        self.dec1 = ConvRelu(64 + num_filters, num_filters)
-#        self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
-#
-#    def forward(self, x):
-#        conv1 = self.conv1(x)
-#        conv2 = self.conv2(self.pool(conv1))
-#        conv3 = self.conv3(self.pool(conv2))
-#        conv4 = self.conv4(self.pool(conv3))
-#        conv5 = self.conv5(self.pool(conv4))
-#
-#        center = self.center(self.pool(conv5))
-#
-#        dec5 = self.dec5(torch.cat([center, conv5], 1))
-#
-#        dec4 = self.dec4(torch.cat([dec5, conv4], 1))
-#        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
-#        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
-#        dec1 = self.dec1(torch.cat([dec2, conv1], 1))
-#        return self.final(dec1)
-#
 
