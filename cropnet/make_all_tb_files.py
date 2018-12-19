888a1314eec0aaf373530ab4202185e687aca832
Matt Phillips <matt.phillips@kitware.com>
2018-12-18 22:17:53 -0500

    Code to generate all tb_chips files

--- cropnet/make_all_tb_files.py
+++ cropnet/make_all_tb_files.py
@@ -10,7 +10,7 @@ import os
 import platform
 import shutil
 
-from make_images import make_and_save_cdl, make_and_save_cdl,\
+from make_images import make_and_save_hls, make_and_save_cdl,\
         make_and_save_tbchips
 from utils import get_chip_bbox
 
@@ -30,24 +30,30 @@ g_hls_stub = "hls_cls_ark_time%d_band%d_%d_%d_%d_%d.npy" # TODO "ark"
 
 g_chip_size = 1000  # 1000x1000 pix
 g_region_subdirs = ["ark", "sd", "ohio", "vai"]
+g_cdl_file_names = { "ark" : "cdl_2016_neAR.tif",
+        "sd" : "cdl_2016_seSD.tif",
+        "ohio" : "cdl_2016_nwOH.tif",
+        "vai" : "cdl_2016_vai_crop.tif"}
 
 def main(args):
+    ground_truth_dir = pj(args.input_supdir, "ground_truth")
     for subdir in g_region_subdirs:
         input_dir = pj(args.input_supdir, subdir)
         output_dir = pj(args.output_supdir, subdir)
         if pe(output_dir):
             shutil.rmtree(output_dir)
         os.makedirs(output_dir)
-        gt_file = pj(args.ground_truth_dir, cdl_file)
-            # TODO select cdl file based on region
+        cdl_file_name = g_cdl_file_names[subdir]
+        gt_file = pj(ground_truth_dir, cdl_file_name)
+        hls_dir = pj(args.input_supdir, subdir)
         for i in range(3): # TODO
             xstart = i*g_chip_size
             for j in range(3): # TODO
                 ystart = j*g_chip_size
                 bbox = get_chip_bbox(xstart, ystart, g_chip_size)
-                make_and_save_hls(output_dir, args.hls_dir, bbox)
-                make_and_save_cdl(output_dir, args.ground_truth_file, bbox)
-                    make_and_save_tbchips(output_dir, bbox)
+                make_and_save_hls(output_dir, hls_dir, bbox)
+                make_and_save_cdl(output_dir, gt_file, bbox)
+                make_and_save_tbchips(output_dir, bbox)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
